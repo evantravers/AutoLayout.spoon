4dac3969891a1140a321f44d9ae5e454e25b3393
Evan Travers <evantravers@gmail.com>
2022-01-12 16:39:28 -0600

    Yank out everything

--- init.lua
+++ init.lua
@@ -9,31 +9,11 @@
 --- scale theoretically.
 ---
 --- When you start it, it starts the watcher. You can also trigger an autolayout
---- by calling module.autoLayout()
+--- manually by calling module.autoLayout()
 ---
---- Expects a table with a key for `applications` with sub-tables with a
---- bundleID and a set of layouts following this pattern:
 ---
---- layouts = {
----   {
----     <window title string or nil>,
----     <int of preferred monitor>,
----     <layout from hs.layout>
----   }
---- }
----
---- Example:
---- config.applications = {
----   ['com.brave.Browser'] = {
----     bundleID = 'com.brave.Browser',
----     layouts = {
----       {"Meet - ", 2, hs.layout.maximized}
----     }
----   }
---- }
 
 local m = {}
-local fn = hs.fnutils
 
 m.num_of_screens = 0
 m.whichScreen = function(num)
@@ -49,48 +29,20 @@ end
 -- Evaluates module.config and obeys the layouts.
 -- Includes any layouts in module.config.layout as overrides.
 function m:autoLayout()
-  local space = nil
-  if hs.settings.get('headspace') then
-    space = hs.fnutils.find(m.config.spaces, function(s)
-      return s.text == hs.settings.get('headspace').text
-    end)
-  end
+  print(hs.inspect(m.layouts))
+  hs.layout.apply(m.layouts, string.match)
 
-  if space and space.layouts then
-    hs.layout.apply(fn.concat(m.config.layouts, space.layouts), string.match)
-  else
-    hs.layout.apply(m.config.layouts, string.match)
-  end
+  return self
+end
+
+function m:setLayouts(layouts)
+  m.layouts = layouts
 
   return self
 end
 
 -- initialize watchers
-function m:start(config_table)
-  m.config = config_table
-
-  local layouts = {}
-  fn.map(m.config.applications, function(app_config)
-    local bundleID = app_config['bundleID']
-    if app_config.layouts then
-      fn.map(app_config.layouts, function(rule)
-        local title_pattern, screen, layout = rule[1], rule[2], rule[3]
-        table.insert(layouts,
-          {
-            hs.application.get(bundleID), -- application name
-            title_pattern,                -- window title
-            m.whichScreen(screen),        -- screen
-            layout,                       -- layout
-            nil,
-            nil
-          }
-        )
-      end)
-    end
-  end)
-
-  m.config.layouts = layouts
-
+function m:start()
   m.watcher = hs.screen.watcher.new(function()
     if m.num_of_screens ~= #hs.screen.allScreens() then
       m.autoLayout()
